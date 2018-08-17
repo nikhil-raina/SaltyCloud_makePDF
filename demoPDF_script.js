51f0cc2c43a90b3c1c2cee14a3781a66bc851331
Nikhil Raina <nxr5013@g.rit.edu>
2018-08-17 13:51:13 -0500

    Finished commenting and writing possible implementation ideas for the code to match the ISORA Product class tags and its animation, along with the working. #7-#26 #37-#50 #54 #64-#66 #70-#76 #97 #124-#132 #145-#153

--- demoPDF_script.js
+++ demoPDF_script.js
@@ -4,6 +4,26 @@ function setUpEvents() {
      * main part of the entire page, that contains all the information, the graphs and the further details
      * all of it comes under the ID=content. Therefore I have already predefined it here with my program to make
      * it a little easier.
+     *
+     * To allow the "expanding" method of each specific ORG_UNIT_QUESTION, you are essentially creating the:
+     *      'ReportQuestionCategoryView', in which you have the 'toggleCategoryDisplay' that allows the "expanding" and
+     *          "shrinking" of the entire unit_question.
+     *
+     *          Over there you are essentially taking the object of the document with the targeted class and performing
+     *          the action.
+     *          In this case there is only one label, but what I was thinking, since the categories are in the:
+     *              <div> tag class=categories-panel pull-right
+     *
+     *          what I was thinking is that under this class, find out the number of all the categories. This could be
+     *          done by:
+     *              let totalNumber = $("class name").length;
+     *
+     *          This line of code would get you the number of categories. The similar concept could be used to identify
+     *          the number of questions in each category, which would be really important when you would be setting up
+     *          the TIME LIMIT, which you will come across later on in the File.
+     *
+     *          But yeah, then by probably running a loop, you could open up each category since you already have the
+     *          number of categories or you could do it simultaneously, if there is a code for that.
      */
     let content = document.getElementById("content");
     let button = document.getElementById("show-more");
@@ -14,9 +34,24 @@ function setUpEvents() {
      *
      * The transition time for the "opening" and "closing" is about 0.5s, which is mentioned on the Style Sheet
      * that you can also view just to get clarity on its functioning.
+     *
+     * This is essentially where the lines of code:
+     *      $(evt.target).removeClass("glyphicon-collapse-up").addClass("glyphicon-collapse-down");
+     *                                          and
+     *      $(evt.target).removeClass("glyphicon-collapse-down").addClass("glyphicon-collapse-up");
+     *
+     *   Is being used. So you could implement that same method while "expanding" all the categories.
+     *
+     *   The only problem is that, since the PDF is not made separately and instead it is being generated from the file
+     *   itself, all the "expanding" would be occurring when the button "Print PDF" or something like that, has been
+     *   pressed. As after all, a PDF is pretty much like a 'screenshot' of the required data, but just in a more of a
+     *   raw like format.
+     *   My initial approach was to make the PDF right from scratch, with would have prevented the all the animation to
+     *   show, but that was highly complicated and extremely time inefficient as all the data had to be re-modelled.
      */
     button.onclick = function () {
         if(content.className === "open"){
+            // shrink
             content.className = "";
             button.innerText = "Show More";
         }
@@ -26,9 +61,19 @@ function setUpEvents() {
             button.innerText = "Show Less";
         }
 
+        /**
+         * Function call specifically for the PDF creation.
+         */
         timeLoop();
     };
 }
+
+/**
+ * Function that is mainly responsible for the PDF creation. Calls a nested function in itself while ensuring that a
+ * specific amount of "TIME LIMIT" has been surpassed.
+ *
+ * The TIME LIMIT is extremely crucial.
+ */
 function timeLoop(){
     setTimeout(function () {
 
@@ -49,7 +94,7 @@ function timeLoop(){
          *
          * In order for this to work for the execution of the ISORA program, you might have to change the line of
          * code to:
-         *          ...drawDOM($("#content")).then(...)
+         *          ...drawDOM($("#content")).then(...){...}           #73
          *
          * Since I am not able to run the code for the ISORA code myself, I cant help you with this check, but this is
          * a small thing, so I am certain that a line of code would be fine to change.
@@ -77,6 +122,15 @@ function timeLoop(){
          * So in order to avoid the awkwardness of the "wait" feeling, I added an alert at the beginning of this
          * function to make it appear that "there is some background work occurring".
          *
+         * The mathematical approach, at least I think could be like a start, could possibly be:
+         *      Total # of Q. from all categories = x
+         *      Transition effect of an individual category = k (as it would most likely remain constant)
+         *
+         *      Total TIME LIMIT = k*x*1000
+         *
+         *      Also, about 30000 is roughly 30 seconds, so in case the math is completely flawed then you could
+         *      look at it more objectively as per each category and come up with a common number I guess...
+         *
          */
     }, 900)
 }
@@ -88,6 +142,15 @@ function timeLoop(){
  * This isn't an important part of the program, but you could think of it like even if the button is pressed, to allow
  * the PDF creation to take place, it would just ensure that the entire document is completely ready before it would go
  * the execution of the PDF.
+ *
+ * This could possibly act like a "check" function. Something like that would check if all the categories are
+ * "expanded", only then continue forward with generating the PDF. In that way, this should be getting called instead of
+ * it calling the function to make the PDF.
+ *
+ * Again, this was just to cater to my program and the way I have coded.
+ *
+ * I hope that I have made it as easy as I can for you in order to implement this functionality into the Reports section
+ * of the ISORA Product. If you require any further explanation, I am always available.
  *
  */
 window.onload = function () {
